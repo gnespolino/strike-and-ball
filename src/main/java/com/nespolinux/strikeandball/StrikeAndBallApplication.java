74b85e966f255426cbe195c0c5866b25c3c35f69
Giuseppe Nespolino <g.nespolino@gmail.com>
2024-12-17 17:03:10 +0100

    Initial commit

---
+++ src/main/java/com/nespolinux/strikeandball/StrikeAndBallApplication.java
@@ -0,0 +1,13 @@
+package com.nespolinux.strikeandball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrikeAndBallApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StrikeAndBallApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nespolinux/strikeandball/StrikeAndBallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nespolinux.strikeandball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrikeAndBallApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
